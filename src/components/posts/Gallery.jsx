1dffaf6fdfa9e47329b7e5f7eefe6458676508fe
mrhanna <hanna.michaelr@gmail.com>
2024-07-31 14:40:16 -0500

    Galleries render arrows only when prev/next is available

--- src/components/posts/Gallery.jsx
+++ src/components/posts/Gallery.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 import Slider from 'react-slick';
 import Image from 'next/image';
@@ -14,18 +14,21 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 // import "./slickfix.css"; //.slick-track { line-height: 0 }
 
-const slickSettings = {
-    dots: true,
-    infinite: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-};
-
 const Gallery = ({
     media,
 }) => {
     const slider = useRef(null);
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const slickSettings = {
+        dots: true,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        beforeChange: (i, next) => { setCurrentSlide(next) },
+    };
 
     const images = media?.map(({attributes, id}) => {
         if (attributes.mime.indexOf('image') === 0) {
@@ -46,10 +49,7 @@ const Gallery = ({
         else if (attributes.mime.indexOf('video') === 0) {
             return (
                 <div className="w-full aspect-video relative text-center" key={id}>
-                    <video className="h-full inline-block" controls>
-                        <source type={attributes.mime} src={`http://localhost:1337${attributes.url}`} />
-                        This browser dpes not support video
-                    </video>
+                    <FeedVideoPlayer src={`http://localhost:1337${featuredMedia.data.attributes.url}`} caption={caption} />
                 </div>
             );
         }
@@ -62,14 +62,18 @@ const Gallery = ({
                     {images}
                 </Slider>
             }
+            
+            { currentSlide > 0 &&
+                <button className="absolute z-30 top-0 bottom-0 left-4" onClick={() => { slider?.current?.slickPrev() }}>
+                    <FaChevronLeft className="w-[24px] h-[24px]" />
+                </button>
+            }
 
-            <button className="absolute z-30 top-0 bottom-0 left-4" onClick={() => { slider?.current?.slickPrev() }}>
-                <FaChevronLeft className="w-[24px] h-[24px]" />
-            </button>
-
-            <button className="absolute z-30 top-0 bottom-0 right-4" onClick={() => { slider?.current?.slickNext() }}>
-                <FaChevronRight className="w-[24px] h-[24px]" />
-            </button>
+            { currentSlide < media.length - 1 &&  
+                <button className="absolute z-30 top-0 bottom-0 right-4" onClick={() => { slider?.current?.slickNext() }}>
+                    <FaChevronRight className="w-[24px] h-[24px]" />
+                </button>
+            }
         </div>
     )
 }
