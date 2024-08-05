ed63f94c84dbe5bb59b68b1e2d708bb5fba47afd
Michael Hanna <hanna.michaelr@gmail.com>
2024-08-05 10:03:28 -0500

    Renamed "place" to "places." Changed sidebar padding

--- src/app/Sidebar.jsx
+++ src/app/Sidebar.jsx
@@ -6,7 +6,7 @@ import Navbar from './Navbar';
 const Sidebar = () => {
     return (
         <div className="w-64 relative shrink-0">
-            <div className="flex flex-col sticky h-screen bg-white top-0 bottom-0 px-8 py-4 text-center gap-8 items-stretch">
+            <div className="flex flex-col sticky h-screen bg-white top-0 bottom-0 p-8 text-center gap-8 items-stretch">
                 <div className="text-center">
                     <Image
                         src="/logo.png"
